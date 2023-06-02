33da60d0562638c5fd1c16c5197214956b4e85b3
Aki <ancgrouptx@gmail.com>
2023-06-01 22:54:25 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let Drinks = ["Oleato","Hot Coffes","Hot Teas", "Hot Drinks","Frappucchino Blended", "Cold Coffees", "Iced Teas", "Cold Drinks"]
+let Food = ["Hot Breakfast", "Oatmeal & Yogurt", "Bakery", "Lunch", "Snacks & Sweets"]
+let AtHomeCoffee = ["Whole Bean", "VIA Instant"]
+let Merchandise = ["Cold Cups","Tumblers","Mugs","Water Bottles"]
+let GiftCards = ["Happy Birthday", "Thank You", "Traditional"]
